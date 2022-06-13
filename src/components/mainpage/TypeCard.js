27cdfc8c500a2795a09574805e3504c4605061e3
Fareks <mselinkor@gmail.com>
2022-06-13 14:59:44 +0300

    created category-card on mainpage

--- src/components/mainpage/TypeCard.js
+++ src/components/mainpage/TypeCard.js
@@ -1,12 +1,24 @@
 import './content-section.css'
-
-
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const TypeCard = ({img,text,link}) => {
     return (
-        <div className="type-card-container">
+        <Link to={link}>
+            <motion.div 
+            initial={{y:0}}
+            whileHover={{y:'4px'}}
+            className="type-card-container">
+                
+                <img src={img} alt=""  width="448" height="550" className='type-card-img'/>
+                
+                <p className="type-card-text">
+                    {text}
+                </p>
 
-        </div>
+            </motion.div>
+        </Link>
+        
     );
     };
     
